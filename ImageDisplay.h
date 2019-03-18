f6a9a208d029c366a1c28fc8638c1ae520078b61
David Post <david@ohyonghao.com>
2019-03-18 12:31:20 -0700

    Rearrange things to look pretty
    Mostly just whitespace.
    
    Signed-off-by: David Post <david@ohyonghao.com>

--- ImageDisplay.h
+++ ImageDisplay.h
@@ -16,7 +16,7 @@ class ImageDisplay : public QWidget
 {
     Q_OBJECT
 public:
-    explicit ImageDisplay(QString filename, int isovalue=57, int stepsize = 5, bool useBinaryInter = true, QWidget *parent = nullptr);
+    explicit ImageDisplay(QString filename, int isovalue=ISOVALUE, int stepsize = STEPSIZE, bool useBinaryInter = true, QWidget *parent = nullptr);
     QSizePolicy sizePolicy(){return imageLabel->sizePolicy();}
     QSize size(){return imageLabel->size();}
 private:
@@ -36,22 +36,22 @@ private slots:
     void loadImage(const QByteArray &image);
 
 public slots:
-    void BinaryGray(){processor.QueueProcess(std::mem_fn(&ImageProcessor::BinaryGray));}
-    void Pixelate(){processor.QueueProcess(std::mem_fn(&ImageProcessor::Pixelate));}
-    void Blur(){processor.QueueProcess(std::mem_fn(&ImageProcessor::Blur));}
-    void Contour(){processor.QueueProcess(std::mem_fn(&ImageProcessor::Contour));}
-    void CelShade(){processor.QueueProcess(std::mem_fn(&ImageProcessor::CelShade));}
-    void ScaleDown(){processor.QueueProcess(std::mem_fn(&ImageProcessor::ScaleDown));}
-    void ScaleUp(){processor.QueueProcess(std::mem_fn(&ImageProcessor::ScaleUp));}
-    void Rot90(){processor.QueueProcess(std::mem_fn(&ImageProcessor::Rot90));}
-    void Rot180(){processor.QueueProcess(std::mem_fn(&ImageProcessor::Rot180));}
-    void Rot270(){processor.QueueProcess(std::mem_fn(&ImageProcessor::Rot270));}
+    void BinaryGray()  {processor.QueueProcess(std::mem_fn(&ImageProcessor::BinaryGray));}
+    void Pixelate()    {processor.QueueProcess(std::mem_fn(&ImageProcessor::Pixelate));}
+    void Blur()        {processor.QueueProcess(std::mem_fn(&ImageProcessor::Blur));}
+    void Contour()     {processor.QueueProcess(std::mem_fn(&ImageProcessor::Contour));}
+    void CelShade()    {processor.QueueProcess(std::mem_fn(&ImageProcessor::CelShade));}
+    void ScaleDown()   {processor.QueueProcess(std::mem_fn(&ImageProcessor::ScaleDown));}
+    void ScaleUp()     {processor.QueueProcess(std::mem_fn(&ImageProcessor::ScaleUp));}
+    void Rot90()       {processor.QueueProcess(std::mem_fn(&ImageProcessor::Rot90));}
+    void Rot180()      {processor.QueueProcess(std::mem_fn(&ImageProcessor::Rot180));}
+    void Rot270()      {processor.QueueProcess(std::mem_fn(&ImageProcessor::Rot270));}
+    void toggleBinary(){processor.QueueProcess(std::mem_fn(&ImageProcessor::toggleBinary));}
+    void LoadImage()   {processor.QueueProcess(std::mem_fn(&ImageProcessor::LoadImage));}
+    void GrayScale()   {processor.QueueProcess(std::mem_fn(&ImageProcessor::GrayScale));}
     void setIsovalue(int isovalue){ processor.setIsovalue(isovalue);}
     void setStepSize(int stepsize){ processor.setStepSize(stepsize);}
     void setBinaryInter(bool usebininter){processor.setBinaryInter(usebininter);}
-    void toggleBinary(){processor.QueueProcess(std::mem_fn(&ImageProcessor::toggleBinary));}
-    void LoadImage(){processor.QueueProcess(std::mem_fn(&ImageProcessor::LoadImage));}
-    void GrayScale(){processor.QueueProcess(std::mem_fn(&ImageProcessor::GrayScale));}
     void save();
 };
 
